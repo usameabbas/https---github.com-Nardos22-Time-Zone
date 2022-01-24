d1a0dae62577c0144d1f1f0e8e0c177ee9f66f59
usameabbas <89979453+usameabbas@users.noreply.github.com>
2022-01-24 23:52:16 +0100

    header.

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+// src/Title.js
+
+import React from 'react';
+import './Title.css';
+
+function Title() {
+  return (
+
+    <div className="Title">
+      <h1>TIMEZONE</h1>
+
+      <div className="Title-Subtitle"></div>
+
+    </div>
+  )
+}
+
+export default Title
+
+function MyComp() {
+  // Good! Single line
+  return <h1>Hello World</h1>
+}
+
